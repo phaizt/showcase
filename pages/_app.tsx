a7964cd6381f69a449bdc13e05262be38ad1ee09
faiz <faizrizky20@gmail.com>
2022-07-13 17:47:09 +0700

    add meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import type { AppProps } from "next/app"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Layout from "components/layout/Layout"
 import { Provider } from "react-redux"
-import store from "store/main-store";
+import store from "store/main-store"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Provider store={store}>
+            <Head>
+                <title>showcase</title>
+                <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+            </Head>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
